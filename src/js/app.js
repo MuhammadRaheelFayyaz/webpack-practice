8e9cd8ede076f19602c108fb53710c2e36aca330
ali muhammad <ali.muhammad@paid.com>
2019-12-10 13:19:42 -0500

    All about entry, output, module & loaders as well as plugins

--- src/js/app.js
+++ src/js/app.js
@@ -1,26 +1,29 @@
+import "../css/main.css";
+import "../css/input-elements.css";
+import { secretButton, secretParagraph } from "./dom-loader";
 var showSecret = false;
 
-secretButton.addEventListener('click', toggleSecretState);
+secretButton.addEventListener("click", toggleSecretState);
 updateSecretParagraph();
 
 function toggleSecretState() {
-    showSecret = !showSecret;
-    updateSecretParagraph();
-    updateSecretButton()
+  showSecret = !showSecret;
+  updateSecretParagraph();
+  updateSecretButton();
 }
 
 function updateSecretButton() {
-    if (showSecret) {
-        secretButton.textContent = 'Hide the Secret';
-    } else {
-        secretButton.textContent = 'Show the Secret';
-    }
+  if (showSecret) {
+    secretButton.textContent = "Hide the Secret";
+  } else {
+    secretButton.textContent = "Show the Secret";
+  }
 }
 
 function updateSecretParagraph() {
-    if (showSecret) {
-        secretParagraph.style.display = 'block';
-    } else {
-        secretParagraph.style.display = 'none';
-    }
+  if (showSecret) {
+    secretParagraph.style.display = "block";
+  } else {
+    secretParagraph.style.display = "none";
+  }
 }
